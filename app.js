6b84f88632162d5682d5ba0cf967b76755574e7f
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-18 14:11:33 +0530

    Setup project connectivity and install packages

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const Mongo_url="mongodb://127.0.0.1:27017/wonderNest"
+
+main().then((res)=>{
+    console.log("Database Connected...");
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect(Mongo_url);
+}
+
+app.get("/",(req,res)=>{
+    res.send('hey iam root');
+})
+
+app.listen('8080',(req,res)=>{
+    console.log("Listening Port 8080");
+})
