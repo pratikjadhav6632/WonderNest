bd73dc9d21fe19633b093ae819b0ab47b2d77ef6
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-18 20:04:32 +0530

    Add default image URL to listing model

--- model/listing.js
+++ model/listing.js
@@ -12,6 +12,7 @@ const listingSchema=new Schema({
     },
     images:{
         type:String,
+        default:"https://plus.unsplash.com/premium_photo-1676321688630-9558e7d2be10?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set:(v)=>v===""?"https://plus.unsplash.com/premium_photo-1676321688630-9558e7d2be10?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v
     },
     price:{
