1751f709d0cd036a52ee4d45af6167c8916a43b7
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-18 22:41:29 +0530

    Add initial project files and dependencies, including EJS and async utilities

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Mongo_url="mongodb://127.0.0.1:27017/wonderNest"
-const listing=require("./model/listing.js")
+const Listing=require("./model/listing.js")
+const path=require("path");
+
 
 main().then((res)=>{
     console.log("Database Connected...");
@@ -14,6 +16,21 @@ async function main(){
     await mongoose.connect(Mongo_url);
 }
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded({extended:true}));
+//Index route
+app.get("/listings",async(req,res)=>{
+   let allListing= await Listing.find({});
+    res.render("listings/index.ejs",{allListing});
+
+})
+//Show route 
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let listing= await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+})
 //Testing route
 /*app.get("/test",async (req,res)=>{
     let sampledata=new listing({
