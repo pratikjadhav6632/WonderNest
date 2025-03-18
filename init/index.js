ed49f3bca35a38467937f5b0522e2628f9028d0c
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-18 20:16:53 +0530

    Initialize database connection and seed initial data

---
+++ init/index.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+const initData=require("./data.js");
+const Listing=require("../model/listing.js");
+const Mongo_url="mongodb://127.0.0.1:27017/wonderNest"
+
+main().then((res)=>{
+    console.log("Database Connected...");
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect(Mongo_url);
+}
+
+const initDB=async()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data initialzation successfull...");
+
+};
+
+initDB();
