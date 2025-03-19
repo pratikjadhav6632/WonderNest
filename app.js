9a47a7fd9260966b564ee89a743ca3c217603114
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-19 12:58:25 +0530

    Add method-override dependency and implement listing creation and editing forms

--- app.js
+++ app.js
@@ -1,36 +1,74 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const Mongo_url="mongodb://127.0.0.1:27017/wonderNest"
-const Listing=require("./model/listing.js")
-const path=require("path");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Mongo_url = "mongodb://127.0.0.1:27017/wonderNest"
+const Listing = require("./model/listing.js")
+const path = require("path");
+const methodOverride=require("method-override");
 
 
-main().then((res)=>{
+main().then((res) => {
     console.log("Database Connected...");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 })
 
-async function main(){
+async function main() {
     await mongoose.connect(Mongo_url);
 }
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"))
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 //Index route
-app.get("/listings",async(req,res)=>{
-   let allListing= await Listing.find({});
-    res.render("listings/index.ejs",{allListing});
+app.get("/listings", async (req, res) => {
+    let allListing = await Listing.find({});
+    res.render("listings/index.ejs", { allListing });
 
 })
-//Show route 
-app.get("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    let listing= await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
+
+//New route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs")
 })
+
+//Show route 
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+})
+
+//Create route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listings);
+    newListing.save();
+    res.redirect("/listings");
+})
+
+//Edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    let listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+})
+
+//Update route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listings});
+    res.redirect("/listings");
+})
+
+//Destroy route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
 //Testing route
 /*app.get("/test",async (req,res)=>{
     let sampledata=new listing({
@@ -48,10 +86,10 @@ app.get("/listings/:id",async(req,res)=>{
     res.send("test successfull...");
 })*/
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send('hey iam root');
 })
 
-app.listen('8080',(req,res)=>{
+app.listen('8080', (req, res) => {
     console.log("Listening Port 8080");
 })
