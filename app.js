f0d94ba52d8f9bdad22e2f016624424fb62ecfaf
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-15 13:33:00 +0530

    Fix: add missing semicolon in app.js and remove description from listing cards in index.ejs

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ async function main() {
 }
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 app.use(methodOverride("_method"));
