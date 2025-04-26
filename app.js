06a73b3854c52fcc8c27f23148443f7c368704e1
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-26 23:58:02 +0530

    fix: correct formatting of expires property in sessionOptions

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const sessionOptions={
     resave:false,
     saveUninitialized:true,
     cookie:{
-        expires:(Date.now()*7*24*60*60*1000),
+        expires:Date.now()*7*24*60*60*1000,
         maxAge:7*24*60*60*1000,
         httpOnly:true
     }
