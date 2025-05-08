58a4868ad936d82c8ff98b842c5cabec73adcef7
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-08 15:13:51 +0530

    Implemented auto login after signup

--- routes/user.js
+++ routes/user.js
@@ -5,26 +5,30 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 
 router.get("/signup", (req, res) => {
-    res.render("users/signup.ejs");
+  res.render("users/signup.ejs");
 })
 
 router.post("/signup", wrapAsync(async (req, res) => {
-    try {
-        let { username, email, password } = req.body;
-        let newUser = new User({ username, email });
-        let registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.flash("success", `Welcome to WonderNest ${username}`);
-        res.redirect("/listings");
-
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
+  try {
+    let { username, email, password } = req.body;
+    let newUser = new User({ username, email });
+    let registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", `Welcome to WonderNest ${username}`);
+      res.redirect("/listings");
+    })
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
 }));
 
 router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
+  res.render("users/login.ejs");
 });
 
 router.post("/login",
@@ -38,12 +42,12 @@ router.post("/login",
   }
 );
 
-router.get("/logout",(req,res,next)=>{
-  req.logout((err)=>{
-    if(err){
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
       return next(err);
     }
-    req.flash("success","your now logout!");
+    req.flash("success", "your now logout!");
     res.redirect("/listings");
   })
 })
