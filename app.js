bf366f1b0fa9b8d1ad0e12e0c3655a353ef0888d
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-13 11:36:47 +0530

    Refactor session and route imports for improved readability; update review deletion form to conditionally render based on user ownership

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const path = require("path");
 const methodOverride = require("method-override");
 const engine = require("ejs-mate");
 const ExpressError = require("./utils/ExpressErr.js");
-const session=require("express-session");
-const flash=require("connect-flash");
-const passport=require("passport");
-const LocalStrategy=require("passport-local");
-const User=require("./model/user.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./model/user.js");
 
 
-const listing=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
-const user=require("./routes/user.js");
+const listing = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+const user = require("./routes/user.js");
 
 main().then((res) => {
     console.log("Database Connected...");
@@ -30,20 +30,20 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine("ejs", engine);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
-const sessionOptions={
-    secret:"MySecret",
-    resave:true,
-    saveUninitialized:true,
-    cookie:{
-        expires:Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge:7*24*60*60*1000,
-        httpOnly:true
+const sessionOptions = {
+    secret: "MySecret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true
     }
 };
 
@@ -61,10 +61,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.success=req.flash("success");
-    res.locals.error=req.flash("error");
-    res.locals.currUser=req.user;
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
     next();
 });
 
@@ -77,9 +77,9 @@ app.use((req,res,next)=>{
 //     res.send(registeredUser);
 // })
 
-app.use("/listings",listing);
-app.use("/listings/:id/reviews",reviews);
-app.use("/",user);
+app.use("/listings", listing);
+app.use("/listings/:id/reviews", reviews);
+app.use("/", user);
 
 
 app.all("*", (req, res, next) => {
@@ -88,7 +88,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let { status = 500, message = "Something went wrong" } = err;
-    res.status(status).render("Error/err.ejs",{message});
+    res.status(status).render("Error/err.ejs", { message });
     // res.status(status).send(message);
 });
 
