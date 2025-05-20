1ec865001db241227f7ab9b25aa9e76e8679ea77
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-20 23:46:57 +0530

    Refactor tax toggle functionality: move script from index.ejs to script.js for better organization

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,17 @@
       }, false)
     })
   })()
+
+  // index-page (tax-toggler)
+  let taxSwitch = document.getElementById("switchCheckDefault");
+    taxSwitch.addEventListener("click", () => {
+      let taxInfo = document.getElementsByClassName("tax-info");
+      for (info of taxInfo) {
+        if (info.style.display != "inline") {
+          info.style.display = "inline"
+        } else {
+          info.style.display = "none"
+        }
+
+      }
+    })
