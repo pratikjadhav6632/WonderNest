c058c182c3a04a7d9f53dcbd2d1edd455d324a8a
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-22 21:19:59 +0530

    fix: update map style to use environment variable for map key

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 const map = new maplibregl.Map({
         container: 'map',
-        style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+        style:map_key,
         center: [0,0],
         zoom: 2
     });
